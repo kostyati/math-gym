3af65969b20b9d9fcab4d379ec1a7437d1f0361f
Kos <kostyatitov@gmail.com>
2025-03-25 16:18:13 -0400

    springboot project inintiated

---
+++ backend/math-gym-backend/src/main/java/com/mathgym/math_gym_backend/MathGymBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mathgym.math_gym_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathGymBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathGymBackendApplication.class, args);
+	}
+
+}
---
+++ backend/math-gym-backend/src/test/java/com/mathgym/math_gym_backend/MathGymBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathgym.math_gym_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathGymBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
